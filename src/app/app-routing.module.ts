fc2a1dead5ff2ce3da9d7df8ff87bd65a10fb3db
Lorena Silva <lorenagrazy@yahoo.com.br>
2024-01-13 14:12:36 -0300

    Update

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { EducationComponent } from './components/education/education.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { SkillsComponent} from './components/skills/skills.component';
+import { WorkComponent } from './components/work/work.component';
+import { LayoutComponent } from './components/layout/layout.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'skills', component: SkillsComponent },
+      { path: 'education', component: EducationComponent },
+      { path: 'work', component: WorkComponent },
+      { path: 'experience', component: ExperienceComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+      { path: '**', redirectTo: '/home' },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { EducationComponent } from './components/education/education.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { RouterModule } from '@angular/router';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    EducationComponent,
+    ExperienceComponent,
+    NavbarComponent,
+    SkillsComponent,
+    LayoutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    RouterModule,
+    FontAwesomeModule
+    
+  
+    
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+  
+})
+export class AppModule { }
