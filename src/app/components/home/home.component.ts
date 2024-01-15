d399bc46d47feaa1e600d6298466d5a537283393
Lorena Silva <lorenagrazy@yahoo.com.br>
2024-01-14 21:22:42 -0300

    Ultimo update do dia

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,10 +4,9 @@ import Typed from 'typed.js';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
   ngOnInit() {
     this.loadParticlesConfig();
     this.initTypedJS();
@@ -18,16 +17,23 @@ export class HomeComponent implements OnInit {
     const configPath = 'assets/particles-config.json';
 
     fetch(configPath)
-      .then(response => response.json())
-      .then(config => {
+      .then((response) => response.json())
+      .then((config) => {
         (window as any).particlesJS('particles-js', config);
       })
-      .catch(error => console.error('Error loading particles config:', error));
+      .catch((error) =>
+        console.error('Error loading particles config:', error)
+      );
   }
 
   private initTypedJS() {
     const options = {
-      strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+      strings: [
+        'Software Engineering',
+        'Frontend Development',
+        'Angular Development',
+        'Web Development',
+      ],
       loop: true,
       typeSpeed: 50,
       backSpeed: 25,
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,15 @@
-import { Component, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
   isMenuOpen: boolean = false;
