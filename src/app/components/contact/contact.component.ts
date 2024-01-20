7e2044e2552bd23ab74c1c8dcd031d26d52aa2cf
Lorena Silva <lorenagrazy@yahoo.com.br>
2024-01-20 20:30:08 -0300

    Update

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,53 +1,57 @@
-import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { NetlifyFormsService } from 'src/app/services/netlify-forms.service';
 import { Contact } from './contact';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent  {
+export class ContactComponent implements OnInit {
   
-  contactForm = this.fb.group({
-    name: ['', Validators.required],
-    email: ['', Validators.required],
-    phone: ['', [Validators.required]],  // Corrigido Validators.email para Validators.required
-    message: ['', Validators.required],
-  });
-
-  errorMsg = '';
-
-  constructor(
-    private fb: FormBuilder,
-    private router: Router,
-    private netlifyForms: NetlifyFormsService,
-  ) { }
-
-  onSubmit() {
-    // Criar um objeto do tipo Feedback com as propriedades necessárias
-    const contact: Contact = {
-      name: this.contactForm.value.name ?? '', // ou this.contactForm.value.name || ''
-      email: this.contactForm.value.email ?? '',
-      phone: this.contactForm.value.phone ?? '',
-      message: this.contactForm.value.message ?? '',
-   
-    };
-    
-    this.netlifyForms.submitFeedback(contact).subscribe(
-      () => {
-        this.contactForm.reset();
-        this.router.navigateByUrl('/success');
-      },
-      err => {
-        this.errorMsg = err;
-      }
-    );
+  contactForm: FormGroup;
+  isContactFormSubmitted = false;
+  
+  constructor(private http: HttpClient) {
+    this.contactForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      body: new FormControl('', [Validators.required]),
+    });
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
 
-  closeError() {
-    this.errorMsg = '';
+  onSubmit(evt: SubmitEvent) {
+    evt.preventDefault();
+
+    const formData = this.contactForm.value;
+    // This is important. We need to add the hidden field to make sure Netlify picks up the form submission.
+    formData['form-name'] = 'contact';
+    const headers = new HttpHeaders({
+      Accept: 'text/html',
+      'Content-Type': 'application/x-www-form-urlencoded',
+    });
+
+    this.http
+      .post('/', new URLSearchParams(formData).toString(), { headers, responseType: 'text' })
+      .subscribe(() => {
+        this.isContactFormSubmitted = true;
+      });
+  }
+
+  // functions used in the html template
+  get name() {
+    return this.contactForm.get('name');
+  }
+  get email() {
+    return this.contactForm.get('email');
+  }
+  get body() {
+    return this.contactForm.get('body');
   }
 }
